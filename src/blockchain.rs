f48011d80b7fba03077f4eab33b1dc7e166c15ef
Joseph Lai <jlai24142@gmail.com>
2021-07-23 01:38:13 -0400

    Updating Blockchain struct fields. Adding 'difficulty' parameter to the new() method. Updating display() method. Adding tests and doc comments

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,46 +1,59 @@
-// Blockchain.
+//! A simple blockchain.
 
 use crate::block::Block;
 
-// Blockchain struct.
+/// `Blockchain` object.
+/// 
+/// The `Blockchain` stores:
+/// * A `Vec` containing all `Block`s
+/// * A difficulty level which will impact time spent mining/generating proof of work (the nonce)
+/// * A `Vec` containing SHA256 values of each `Block`
+/// 
 #[derive(Debug)]
 pub struct Blockchain {
+    /// Contains all `Block`s stored in this particular `Blockchain`
     pub chain: Vec<Block>,
+    /// A difficulty level which will impact the time spent mining/generating proof of work (the nonce)
+    pub difficulty: usize,
+    /// Contains SHA256 values of all `Block`s stored in this particular `Blockchain`
     pub transactions: Vec<String>
 }
 
-// Blockchain operations.
+/// `Blockchain` operations.
 impl Blockchain {
-    // Initialize a new blockchain.
-    pub fn new() -> Blockchain {
-        let genesis = Block::new("Genesis Block", "0");
+    /// Initialize a new `Blockchain`.
+    pub fn new(difficulty: usize) -> Blockchain {
+        println!("Creating genesis block");
+        let genesis = Block::new("Genesis Block", difficulty, 0, &"0".repeat(64));
         let genesis_hash = genesis.block_hash.clone().to_string();
 
         Blockchain{
             chain: vec![genesis],
+            difficulty: difficulty,
             transactions: vec![genesis_hash]
         }
     }
 
-    // Add a block to the blockchain.
-    pub fn add_transaction(&mut self, data: String) -> bool {
+    /// Add a new `Block` to the `Blockchain`.
+    pub fn add_transaction(&mut self, data: String) {
         let last_block = &self.chain[self.chain.len() - 1];
         let previous_hash = &last_block.block_hash;
 
-        let new_block = Block::new(&data, previous_hash);
+        let new_block = Block::new(&data, self.difficulty, self.chain.len(), previous_hash);
         self.transactions.push(new_block.block_hash.clone().to_string());
         self.chain.push(new_block);
-
-        true
     }
 
-    // Display blockchain.
+    /// Display the `Blockchain`.
     pub fn display(&self) {
-        println!("TRANSACTIONS\n");
+        println!("TRANSACTIONS");
+        println!("------------\n");
         for transaction in &self.transactions {
             println!("SHA256: {}", transaction);
         }
 
+        println!("\nBLOCKS");
+        println!("------");
         for block in &self.chain {
             block.display();
         }
@@ -53,16 +66,18 @@ mod test_blockchain {
 
     #[test]
     fn test_new() {
-        let blockchain = Blockchain::new();
+        let blockchain = Blockchain::new(1);
 
         assert_eq!(blockchain.chain.len(), 1);
+        assert_eq!(blockchain.transactions.len(), 1);
     }
 
     #[test]
     fn test_add_transaction() {
-        let mut blockchain = Blockchain::new();
+        let mut blockchain = Blockchain::new(1);
         blockchain.add_transaction("test".to_string());
 
         assert_eq!(blockchain.chain.len(), 2);
+        assert_eq!(blockchain.transactions.len(), 2);
     }
 }
